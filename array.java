8bb4b1527427a10b76f97e44ac71195d0f7df526
AidanLau10 <aidanlau10@gmail.com>
2023-10-21 16:39:32 -0700

    first rectangle

--- array.java
+++ array.java
@@ -1,1 +1,17 @@
-public class Main
+public class Array {
+    public static void main(String[] args) {
+        int[][] map = {
+            {0, 2, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 1, 1, 3},
+            {0, 1, 1, 1, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0}
+        };
+        for(int i=0; i < map.length; i++) {
+           
+            for(int j=0; j < map[i].length; j++) {
+                System.out.print(map[i][j] + ' ');
+            } 
+        }
+    }
+}
+
